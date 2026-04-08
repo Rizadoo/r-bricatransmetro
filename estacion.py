b71f94f01037c4db1e5811abe17a66d61856c9d3
John L. Lancheros <jhonlara2007@gmail.com>
2026-04-08 13:22:29 -0500

    Se añadieron las clases que se usaran en el sistema

---
+++ estacion.py
@@ -0,0 +1,5 @@
+class Estación:
+    def __init__(ID,nombre_estacion,ubicacion):
+        self.ID = ID
+        self.nombre_estacion = nombre_estacion
+        self.ubicacion = ubicacion
---
+++ main.py
@@ -0,0 +1,7 @@
+from cliente import Cliente
+from tarjeta import Tarjeta
+from transaccion import Transacción
+from ruta import Ruta
+from bus import Bus
+from estacion import Estación
+from parada import Parada
