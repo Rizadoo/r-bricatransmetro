d8e6aa604ef0ebf7ed5ce007bcc3cec23a12caae
John L. Lancheros <jhonlara2007@gmail.com>
2026-04-12 12:59:49 -0500

    Se añadieron los metodos a la clase ruta.

--- ruta.py
+++ ruta.py
@@ -1,6 +1,18 @@
 class Ruta:
-    def __init__(self,codigo,nombre_ruta,tipo_ruta,tarifa_base):
+    def __init__(self, codigo, nombre_ruta, tipo_ruta, tarifa_base):
         self.codigo = codigo
         self.nombre_ruta = nombre_ruta
-        self.tipo_ruta = tipo_ruta
+        self.tipo_ruta = tipo_ruta #
         self.tarifa_base = tarifa_base
+        self.promedio_espera = 0  # Se añadio un atributo nuevo para el promedio de espera.
+        self.paradas = []         # se añadio una Lista para almacenar las paradas asociadas a esta ruta.
+
+    def establecer_promedio(self, minutos):
+        self.promedio_espera = minutos
+
+    def añadir_parada(self, objeto_parada):
+        self.paradas.append(objeto_parada)
+
+    def mostrar_detalle_ruta(self):
+        # Este método es el que usará la Estación para mostrar los datos
+        return f"Ruta {self.codigo} ({self.tipo_ruta}) - Promedio: {self.promedio_espera} min"
