2d3a508331e29e6fedf55c7d4b74424ce4f3f791
John L. Lancheros <jhonlara2007@gmail.com>
2026-04-12 13:00:14 -0500

    Se añadieron los metodos a la clase tarjeta.

--- tarjeta.py
+++ tarjeta.py
@@ -1,6 +1,29 @@
 class Tarjeta:
-    def __init__(self, uid,estado,cantidad_pasajes,saldo):
-        self.id = uid
-        self.estado = estado
-        self.cantidad_pasajes = cantidad_pasajes
-        self.saldo = saldo
+    def __init__(self, id_tarjeta, saldo_inicial):
+        # Atributos de instancia
+        self.id_tarjeta = id_tarjeta
+        self.saldo = saldo_inicial
+        self.limit_max = 150000  # Ahora definido correctamente dentro del constructor
+
+    def consultar_saldo(self):
+        return self.saldo
+
+    def validar_recarga(self, monto):
+        # Verifica que no sea negativo y que no supere el tope de 150k
+        if 0 < monto <= (self.limit_max - self.saldo):
+            return True
+        return False
+
+    def aplicar_recarga(self, monto):
+        # Suma el saldo si la validación es exitosa
+        self.saldo += monto
+
+    def validar_pago(self, costo_total):
+        # Verifica que el saldo no quede en negativo
+        if self.saldo >= costo_total:
+            return True
+        return False
+
+    def aplicar_descuento(self, costo_total):
+        # Resta el valor del saldo
+        self.saldo -= costo_total
