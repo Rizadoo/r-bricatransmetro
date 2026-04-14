c652c3e9e294c7c3faf58c99766a45e48376d41a
John L. Lancheros <jhonlara2007@gmail.com>
2026-04-14 06:54:58 -0500

    Retoque comentarios en transacción, para que se vea más adecuado y ordenado.

--- transaccion.py
+++ transaccion.py
@@ -11,13 +11,13 @@ class Transaccion:
         self.fecha = fecha
 
     def definir_detalle(self, tipo_op, saldo_ant, saldo_nue):
-        """Este método actualiza la información específica de la operación"""
+     #Este método actualiza la información específica de la operación
         self.tipo = tipo_op
         self.saldo_anterior = saldo_ant
         self.saldo_nuevo = saldo_nue
 
     def obtener_resumen(self):
-        """Retorna el diccionario para que el Main lo imprima fácil"""
+    # Este método devuelve un resumen de la transacción, ideal para mostrar en un recibo o historial.
         return {
             "ID Transacción": self.id,
             "Usuario": self.cliente.nombre,
